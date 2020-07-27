2d4ccc13ebcc65901f610abba5c029f33bf16bc3
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2020-07-27 09:11:18 +0700

    drivers: fingerprint: Drop the msecs to jiffies conversion
    There's no point in doing this since wake_lock_timeout() expects
    msecs. Seems to fix the "delay" to wake the device when using fp.
    
    * Preferences from https://github.com/laststandrighthere/vimb_kernel_vince/commit/b5d2d08e8cfa50d2748a52d27d3e42ca3c339e92
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/input/fingerprint/gf_spi.c
+++ drivers/input/fingerprint/gf_spi.c
@@ -526,7 +526,7 @@ static irqreturn_t gf_irq(int irq, void *handle)
 	kobject_uevent(&gf_dev->spi->dev.kobj, KOBJ_CHANGE);
 
 	//printk("[GDX_FP] irqreturn: netlink enable \n");
-	wake_lock_timeout(&fp_wakelock, msecs_to_jiffies(WAKELOCK_HOLD_TIME));
+	wake_lock_timeout(&fp_wakelock, WAKELOCK_HOLD_TIME);
 	sendnlmsg(&msg);
 	//printk("[GDX_FP] irqreturn \n");
 	return IRQ_HANDLED;
